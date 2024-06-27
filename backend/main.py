7922e001e79cf425b05637801779abd9a10c94a1
昭弘かいと <erlanggaibr2@gmail.com>
2024-06-28 03:41:40 +0700

    FINAL

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,27 @@
 from flask import Flask
+from flask import request
+from flask import send_from_directory
+from flask import render_template
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.get("/")
+@cross_origin()
 def landing_page():
-    ...
+    return render_template("index.html")
 
-@app.post("/quotes")
+@app.route("/quotes", methods=['POST'])
+@cross_origin()
 def add_quotes():
     saved_quotes = []
     with open("quotes.txt", "r") as f:
         saved_quotes = f.readlines()
 
-    saved_quotes.append("This is a new quote\n")
+    new_quote = str(request.json['quote'])
+    saved_quotes.append(new_quote + "\n")
     
     with open("quotes.txt", "w") as f:
         for i in saved_quotes:
@@ -20,5 +29,8 @@ def add_quotes():
         
     with open("index.txt", "w") as f:
         f.write(str(len(saved_quotes)))
+    
+    with open("currentQuotes.txt", "w") as f:
+        f.write(new_quote)
 
     return {"message": "success"}
---
+++ backend/run.bat
@@ -0,0 +1,1 @@
+flask --app main.py run --host 0.0.0.0
